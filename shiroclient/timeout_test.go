847674455bd643a870a8e9c9c8f1c59e0dd34587
sam-at-luther <sam.wood@luthersystems.com>
2023-10-23 16:15:48 -0700

    Properly wrap timeout errors

---
+++ shiroclient/timeout_test.go
@@ -0,0 +1,19 @@
+package shiroclient
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsTimeoutError(t *testing.T) {
+	err := &scError{
+		message: "timeout",
+		code:    1,
+	}
+
+	wrappedErr := fmt.Errorf("wrap call error: %w", err)
+
+	if !IsTimeoutError(wrappedErr) {
+		t.Errorf("IsTimeoutError failed to identify a wrapped timeout error")
+	}
+}
